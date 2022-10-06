2b52faa48c933b9fe1d50444066ff3c1cf5c6bbc
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-10-06 22:37:14 +0100

    feat: re-implement tab groups from Flowbite

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,70 @@
+import { ReactNode } from "react";
+import * as cs from "classnames";
+
+interface GroupProps {
+  children: ReactNode[];
+}
+export function TabGroup({ children }: GroupProps) {
+  let classes = [
+    "hidden",
+    "text-sm",
+    "font-medium",
+    "text-center",
+    "text-gray-500",
+    "rounded-t-md",
+    "divide-x",
+    "divide-gray-200",
+    "sm:flex",
+    "dark:divide-gray-700",
+    "dark:text-gray-400",
+    "first:bg-red-300",
+  ];
+
+  return <ul className={cs(classes)}>{children}</ul>;
+}
+
+interface ItemProps {
+  title: ReactNode;
+  ariaCurrent: string;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+export function TabItem({ title, ariaCurrent, active, onClick }: ItemProps) {
+  let classes = [
+    "inline-block",
+    "p-4",
+    "w-full",
+    "first:rounded-tl-md",
+    "last:rounded-tr-md",
+  ];
+
+  if (!active) {
+    classes.push(
+      "bg-white",
+      "hover:text-gray-700",
+      "hover:bg-gray-50",
+      "focus:outline-none",
+      "dark:hover:text-white",
+      "dark:bg-gray-800",
+      "dark:hover:bg-gray-700",
+      "dark:hover:text-white",
+      "dark:bg-gray-800",
+      "dark:hover:bg-gray-700"
+    );
+  } else {
+    classes.push(
+      "text-blue-700",
+      "bg-gray-100",
+      "focus:outline-none",
+      "dark:bg-gray-700",
+      "dark:text-blue-300"
+    );
+  }
+
+  return (
+    <li className={cs(classes)} onClick={onClick}>
+      {title}
+    </li>
+  );
+}
