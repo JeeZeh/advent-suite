f48c4b4c617dbe4d8c64563f50502ffff30d8c06
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-10-11 20:35:57 +0100

    chore: refactor ResultIcon to use new AggregateEvaluation

--- src/components/ResultIcon.tsx
+++ src/components/ResultIcon.tsx
@@ -1,4 +1,8 @@
-import { AnswerEval } from "../inputs/inputs";
+import {
+  AggregateEvaluation,
+  AnswerEval,
+  getAggregateEvaluation,
+} from "../inputs/inputs";
 import {
   CheckCircleIcon,
   EllipsisHorizontalCircleIcon,
@@ -7,73 +11,64 @@ import {
 } from "@heroicons/react/24/solid";
 import classNames from "classnames";
 
-function IconWrapper({
-  evaluations,
-  useColor,
-  ...rest
-}: ResultIconProps & React.ComponentProps<"svg">) {
-  const baseClass = rest.className;
-
-  if (evaluations == null) {
-    return <EllipsisHorizontalCircleIcon />;
+function getIcon(
+  evaluation: AggregateEvaluation
+): (props: React.SVGProps<SVGSVGElement>) => JSX.Element {
+  switch (evaluation) {
+    case AggregateEvaluation.AllCorrect:
+      return CheckCircleIcon;
+    case AggregateEvaluation.PartialCorrect:
+      return QuestionMarkCircleIcon;
+    case AggregateEvaluation.AllIncorrect:
+      return XCircleIcon;
+    default:
+      return EllipsisHorizontalCircleIcon;
   }
-
-  // Everything is correct
-  if (evaluations.every((e) => e == AnswerEval.Correct)) {
-    return (
-      <CheckCircleIcon
-        {...rest}
-        className={
-          useColor
-            ? classNames("fill-green-600", "dark:fill-green-400", baseClass)
-            : baseClass
-        }
-      />
-    );
-  }
-
-  // Nothing is Incorrect, but not everything is Correct (some Incomplete)
-  if (evaluations.every((e) => e !== AnswerEval.Incorrect)) {
-    return (
-      <QuestionMarkCircleIcon
-        {...rest}
-        className={
-          useColor ? classNames("fill-yellow-400", baseClass) : baseClass
-        }
-      />
-    );
-  }
-
-  return (
-    <XCircleIcon
-      {...rest}
-      className={
-        useColor
-          ? classNames("fill-red-600", "dark:fill-red-400", baseClass)
-          : baseClass
-      }
-    />
-  );
 }
 
-type IconSizes = {
-  sm: "h-4";
-  md: "h-6";
-  lg: "h-8";
+function getColor(evaluation: AggregateEvaluation): string[] {
+  switch (evaluation) {
+    case AggregateEvaluation.AllCorrect:
+      return ["fill-green-600", "dark:fill-green-400"];
+    case AggregateEvaluation.PartialCorrect:
+      return ["fill-yellow-400"];
+    case AggregateEvaluation.AllIncorrect:
+      return ["fill-red-600", "dark:fill-red-400"];
+    default:
+      return [];
+  }
+}
+
+const iconSizes = {
+  sm: "h-4",
+  md: "h-6",
+  lg: "h-8",
 };
 
-
-interface ResultIconProps {
-  evaluations: AnswerEval[] | undefined;
+interface ResultIconProps extends React.SVGProps<SVGSVGElement> {
+  evaluations: AnswerEval[] | AnswerEval | undefined;
   useColor?: boolean;
-  size?: keyof IconSizes;
+  size?: keyof typeof iconSizes;
 }
 /**
  * Determine and return which icon to display for the RunResult evaluation.
  * @returns An SVG with the icon corresponding to the input evaluation result.
  */
 export function ResultIcon(props: ResultIconProps) {
-  let size = props.size ?? "md";
+  const { evaluations, useColor, size, className, ...rest } = props;
 
-  return <IconWrapper {...props} className={size} />;
+  // Collect classes to apply to icon
+  const determinedClasses = [];
+
+  const aggregateEvaluation = getAggregateEvaluation(evaluations);
+  const Icon = getIcon(aggregateEvaluation);
+
+  // Apply color to the icon conditionally
+  if (props.useColor) {
+    determinedClasses.push(...getColor(aggregateEvaluation));
+  }
+
+  determinedClasses.push(iconSizes[props.size ?? "md"]);
+
+  return <Icon {...props} className={classNames(determinedClasses)} />;
 }
