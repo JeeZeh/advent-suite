1a82e518f962d872e70e4550172e16836463e566
Jesse Ashmore <jesse.ashmore97@gmail.com>
2022-10-17 22:15:58 +0100

    refactor: input selector

--- src/components/InputSelector.tsx
+++ src/components/InputSelector.tsx
@@ -7,6 +7,90 @@ import { TabGroup, TabItem } from "./Tabs";
 import ButtonGroup from "flowbite-react/lib/esm/components/Button/ButtonGroup";
 import { RunResult } from "../solutions/utils";
 
+function renderRunButtons(
+  isRunning: boolean,
+  runProblemInputs: (inputs: ProblemInput[]) => void,
+  selectedProblemInput: ProblemInput,
+  problemInputs: ProblemInput[]
+) {
+  return (
+    <div className={cs("flex", "space-x-2")}>
+      <ButtonGroup>
+        <Button
+          color={isRunning ? "info" : "success"}
+          onClick={() => runProblemInputs([selectedProblemInput])}
+        >
+          {isRunning ? (
+            <>
+              <Spinner size="sm" aria-label="Spinner button example" />
+              <span className={cs("pl-3")}>Running...</span>
+            </>
+          ) : (
+            "Run"
+          )}
+        </Button>
+        <Button
+          color={isRunning ? "info" : "light"}
+          onClick={() => runProblemInputs(problemInputs)}
+        >
+          {isRunning ? (
+            <>
+              <Spinner size="sm" aria-label="Spinner button example" />
+              <span className="pl-3">Running...</span>
+            </>
+          ) : (
+            "Run all"
+          )}
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+function renderInputTabs(
+  problemInputs: ProblemInput[],
+  runResults: Map<string, RunResult>,
+  selectedProblemInput: ProblemInput,
+  setSelectedProblemInput: (input: ProblemInput) => void
+) {
+  return (
+    <TabGroup aria-label="Default tabs">
+      {problemInputs.map((p) => (
+        <TabItem
+          key={`option-${p.name}`}
+          title={
+            <div
+              className={cs("flex", "gap-2", "items-center", "justify-center")}
+            >
+              <div>
+                <ResultIcon
+                  evaluations={runResults.get(p.name)?.evaluation}
+                  useColor
+                  size="md"
+                  className="flex-shrink-0"
+                />
+              </div>
+              <div
+                className={cs(
+                  "text-ellipsis",
+                  "overflow-hidden",
+                  "whitespace-nowrap",
+                  "flex-shrink"
+                )}
+              >
+                {p.name}
+              </div>
+            </div>
+          }
+          ariaLabel={p.name}
+          active={selectedProblemInput?.name == p.name}
+          onClick={() => setSelectedProblemInput(p)}
+        />
+      ))}
+    </TabGroup>
+  );
+}
+
 interface InputSelectorProps {
   isRunning: boolean;
   problemInputs: ProblemInput[];
@@ -25,47 +109,22 @@ function InputSelector({
 }: InputSelectorProps) {
   return (
     <>
-      <div className={cs("flex", "flex-col", "gap-2", "w-full")}>
+      <div className={cs("flex", "flex-col", "gap-2", "w-auto")}>
         <div
           className={cs("flex", "space-x-4", "items-end", "justify-between")}
         >
-          <div className={cs("flex", "space-x-2")}>
-            <ButtonGroup>
-              <Button
-                color={isRunning ? "info" : "success"}
-                onClick={() => runProblemInputs([selectedProblemInput])}
-              >
-                {isRunning ? (
-                  <>
-                    <Spinner size="sm" aria-label="Spinner button example" />
-                    <span className={cs("pl-3")}>Running...</span>
-                  </>
-                ) : (
-                  "Run"
-                )}
-              </Button>
-              <Button
-                color={isRunning ? "info" : "light"}
-                onClick={() => runProblemInputs(problemInputs)}
-              >
-                {isRunning ? (
-                  <>
-                    <Spinner size="sm" aria-label="Spinner button example" />
-                    <span className="pl-3">Running...</span>
-                  </>
-                ) : (
-                  "Run all"
-                )}
-              </Button>
-            </ButtonGroup>
-          </div>
+          {renderRunButtons(
+            isRunning,
+            runProblemInputs,
+            selectedProblemInput,
+            problemInputs
+          )}
         </div>
       </div>
       <div
         className={cs(
           "flex",
           "flex-col",
-          "w-full",
           "rounded-md",
           "border",
           "border-gray-300",
@@ -75,36 +134,12 @@ function InputSelector({
           "dark:divide-gray-600"
         )}
       >
-        <div className={cs()}>
-          <TabGroup aria-label="Default tabs">
-            {problemInputs.map((p) => (
-              <TabItem
-                key={`option-${p.name}`}
-                title={
-                  <div
-                    className={cs(
-                      "flex",
-                      "space-x-2",
-                      "items-center",
-                      "justify-center"
-                    )}
-                  >
-                    <div className={cs("w-6")}>
-                      <ResultIcon
-                        evaluations={runResults.get(p.name)?.evaluation}
-                        useColor
-                      />
-                    </div>
-                    <div>{p.name}</div>
-                  </div>
-                }
-                ariaLabel={p.name}
-                active={selectedProblemInput?.name == p.name}
-                onClick={() => setSelectedProblemInput(p)}
-              />
-            ))}
-          </TabGroup>
-        </div>
+        {renderInputTabs(
+          problemInputs,
+          runResults,
+          selectedProblemInput,
+          setSelectedProblemInput
+        )}
 
         <div
           className={cs(
